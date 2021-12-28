31d934cf0d5a6ffa1ad98d35e2366b7853b5a54d
Yishan Lee <yishanlee806@gmail.com>
2021-12-28 14:54:46 +0800

    built up a domain method with defdomain inside of *domain* as hash-table

--- io.lisp
+++ io.lisp
@@ -1,17 +1,13 @@
 (defparameter *input* '())
-;; read file to get the object from the hddl file
+;; read file to get the cons from the hddl file
 ; use open to dummy open the file
 (defun read-hddl (filename)
   (let ((stream (open filename)))
-    ; (when stream 
-    ;   (loop for line = (read-line in nil)
-    ;         ;while line do (format t "~a~%" line)
-    ;         while line do (push (cdr line) *input*)
    (when stream 
      (setq *input* '())
      (push (read-file filename) *input*))
     (close stream)))
-;; idea2 
+
 ;; defdomain construction to read item(argument)
 (defstruct problem
   (namep)
@@ -32,6 +28,7 @@
   (actions)
   )
 
+;; <TODO>to get one task easier using defstruct  
 ; ;; tasks
 ; (defstruct task
 ;   parameters precondition effect)
@@ -43,10 +40,8 @@
 ; (defstruct action
 ;   parameters preconditions effect)
 
-; destructuring-bind for the datasets separation (see reference on "practical common lisp- destructuring-bind")
 
-;;list domain's elements to get individual data
-;; need another function to retrieve the datasets of each element
+;;<TODO>method to get individual data of one domain
 ; {:namedo `(,@(mapcar #'cadadr *input*))
 ;                                 :requirements `(,@(mapcar #'caddr *input*))
 ;                                 :types `(,@(mapcar #'cadddr *input*)) 
@@ -54,6 +49,8 @@
 ;                                 :tasks `(,@(mapcar #'cadr `(,@(mapcar #'cddddr *input*))))
                                 ;:methods
                                 ;:actions  }
+                                
+;; <finished> with a hash table to retrieve domains as struct
 (defvar *domains* (make-hash-table)) 
 (defun defdomain (name &optional requirement type predicate task method action) 
         (setf (gethash name *domains*) 
@@ -65,15 +62,5 @@
                                 :tasks task
                                 :methods method
                                 :actions action
-                          ; amend as (let* (key (mapcar ))
                         )  ))
 
-
-;; idea3 
-;; build up keywords as formation for basis structure
-; (defconst hddl-keywords 
-;   (list (cons (regexp-opt '(":requirements" ":types" ":constants" ":predicates"
-; 			    ":action" ":domain" ":parameters" ":effect"
-; 			    ":precondition" ":objects" ":init" ":goal"
-; 			    ":functions" ":duration" ":condition" ":derived"
-;			    ":metric") t))))
