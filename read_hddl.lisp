13e5686f3aa14b4e983bd6615ad862550e6c4ad1
Alisa Muensterberg <alisa-veronique.muensterberg@stud.uni-bamberg.de>
2022-01-23 22:52:43 +0100

    read-problem works now, too; only thing left to do is proper typing and making code more readable/efficient!

--- read_hddl.lisp
+++ read_hddl.lisp
@@ -1,22 +1,13 @@
 #|
-HDDL-einlesen: DomÃ¤ne + Problem!
+TODO:
+- typing: find types in object-list, parameters etc. and save objects and their type to pairs 
+           for easy access; maybe even use type-list (at least for domain);use hddl-object for this as well
+- make one big function to read both files such that type-list can be used for typing in both cases?
 
-read-domain -> *tasks*, *actions*, *methods*, *types*, *predicates*
-read-problem -> *goal-tasks*, *status*, *objects*
-			
- -> Domäne: - Liste mit tasks (Name, Parameter) -> name, *parameter*, *task-constraints* (-> alle tasks, die vor einer task erledigt sein mÃ¼ssen)              
-			 - Liste mit Aktionen (Namen, Parameter, zugehÃ¶rige task,preconditions, Effekte) -> name, *parameter*, task, *pre*, *effect+*, *effect-*  
-			 - Liste mit Methoden (Namen, Parameter,zugehÃ¶rige task,preconditions, Subtasks) -> name, *parameter*, task, *pre*, *subtasks*
-			 - types als einzelnes?
-			 - PrÃ¤dikate als einzelnes?
-			 - ordering?
-			
- -> Problem: - Liste mit Objekten? -> (name, type)
-			  - Status (:init) -> *status* -> (predicate, True/False)
-			  - tasks -> was wir machen wollen ("Ziel") -> name, *parameter*
+- find more elegant solution for htn-separation into tasks, ordering, constraints in read-hddl-problem
 |#
 
-;; Strukturen für die einzelnen Elemente der Domäne/Problem + die Domäne/Problem selbst
+;; structures for the elements of a hddl-domain or hddl-problem and for the problem and domain as a whole
 (defstruct hddl-predicate name parameters)
 (defstruct hddl-method name parameters task subtasks);;Achtung: Subtasks können auch ordered-subtasks heißen
 (defstruct hddl-action name parameters preconditions neg-effects pos-effects)
@@ -25,13 +16,13 @@ read-problem -> *goal-tasks*, *status*, *objects*
 (defstruct hddl-object name type)
 (defstruct hddl-problem name domain objects tasks ordering constraints init-status)
 
-;; aus Projekt-Sitzung, Vorschlag von Prof. Wolter zum Einlesen:
+;; read in a file
 (defun read-file (filename)
   (with-open-file (in filename)
-    (read in))) ;;liegt schon als Liste vor! bspw. (first *) first des letzten Aufrufs
+    (read in))) 
    
 
-
+;;read in hddl-domain-files and transform them into usable hddl-structures
 (defun read-hddl-domain (filename)
    (declare (optimize debug))
   (print "Reading domain-file...")
@@ -39,6 +30,8 @@ read-problem -> *goal-tasks*, *status*, *objects*
     (error "This function can only read a HDDL file - make sure the file you want to read ends in .hddl! "))
   (unless (search "domain" filename) 
     (error "This function can only read a domain-HDDL file - make sure the file you want to read is the domain-file! "))
+
+  ;;read in file and separate into htn-components:
   (let* ((read-domain (read-file filename))
 	 types 
 	 requirements
@@ -47,7 +40,10 @@ read-problem -> *goal-tasks*, *status*, *objects*
 	 methods 
 	 actions 
 	 (look-up '((:types . types) (:requirements . requirements) (:predicates . predicates) (:task . tasks) (:method . methods) (:action . actions))))
-    (declare (special types requirements predicates tasks methods actions));; lexikalische Deklaration der Variablen
+
+    ;;lexical declaration of variables for matching of defined lists and elements in look-up:
+    (declare (special types requirements predicates tasks methods actions))
+    
     ;; for each element of the domain-list search for its first element in the look-up list and
     ;; push it to the corresponding list (second half of the pair)
     (dolist (element (cddr  read-domain))
@@ -66,19 +62,20 @@ read-problem -> *goal-tasks*, *status*, *objects*
 	  (hddl-actions)
 	  (hddl-predicates)
 	  (domain))
-      ;;setq always returns the last binding
+      
       (setq hddl-requirements
-	    (cdr (car requirements))
+	    (cdr (car requirements)) ;;remove unneccessary key-word from beginning of list
 	    hddl-types
 	    (cdr (car types))
+	    
 	    hddl-tasks
 		(loop for (x name y . parameters) in tasks collect
-		  ;;(declare (ignore x y))
-		  (make-hddl-task :name name :parameters parameters))
+		     (make-hddl-task :name name :parameters parameters))
+		
 	   hddl-methods
 	    (loop for (x name y parameters z task w subtasks) in methods collect
-		  ;;(declare (ignore x y z w))
-		  (make-hddl-method :name name :parameters parameters :task task :subtasks subtasks))
+		 (make-hddl-method :name name :parameters parameters :task task :subtasks subtasks))
+	    
       ;; separate effects into positive and negative effects before building hddl-actions:
       temp-actions
       (loop for action in actions collect
@@ -86,42 +83,38 @@ read-problem -> *goal-tasks*, *status*, *objects*
 	      (effect (car (last action)));; mixed effects
 	      (pos-effects)
 	      (neg-effects))
-	(pprint effect);; debugging
+	  ;;(pprint effect);; debugging
+	  
+	  ;; remove the "and" from effects - unnecessary for future use
 	      (if (equal (string (car effect)) "AND")
-		  (setq effect (cdr effect))) ;; remove the and from effects - unnecessary for future use
+		  (setq effect (cdr effect))) 
+	  
+	  ;;separate effects into pos/neg using keyword "not":
 	      (loop for e in effect do
-		(if (equal (string (car e)) "NOT")
+		(if (equal (string (car e)) "NOT") 
 		    (push (cdr e) neg-effects)
 		    (push e pos-effects)))
-	  (pprint neg-effects)
-	  (pprint pos-effects)
+	  ;;(pprint neg-effects) ;;debugging
+	  ;;(pprint pos-effects) ;;debugging
 	  (setq new-action (append new-action neg-effects pos-effects))
-		new-action))
-		     
+		new-action)) ;; collect the new actions
+
+      ;;use list with separated effects to build hddl-actions
 	  hddl-actions
-	    (loop for (x name y parameters z preconditions w neg-effects pos-effects) in temp-actions collect
-		  ;;(declare (ignore x y z w))
-	   (make-hddl-action :name name :parameters parameters :preconditions preconditions :neg-effects  neg-effects :pos-effects pos-effects))
+	  (loop for (x name y parameters z preconditions w neg-effects pos-effects) in temp-actions collect												    (make-hddl-action :name name :parameters parameters :preconditions preconditions :neg-effects  neg-effects :pos-effects pos-effects))
+	    
 	    hddl-predicates
 	    (loop for (name . parameters) in predicates collect
-			      	(make-hddl-predicate :name name :parameters parameters))
+		  (make-hddl-predicate :name name :parameters parameters))
+
+	    ;;finally combine everything into domain structure:
 	   domain (make-hddl-domain :name (second read-domain) :requirements hddl-requirements
-				      :types hddl-types :predicates hddl-predicates :tasks hddl-tasks
+		    :types hddl-types :predicates hddl-predicates :tasks hddl-tasks
 				    :methods hddl-methods :actions hddl-actions)))))
 			
-		
-;; oder als defstruct :action -> Objekt
-
-;; Datenstruktur für Method etc bauen, element zu method intialisieren;
-;; Datenstruktur an HDDL orientieren (defstruct...) oder auch Klasse
-;; requirements -> z.B. Typing aktiviert erst types
-;; look-up könnte auch mit constructor geschrieben werden zB für Methoden-Datenstruktur
-;; Typing könnte dann zB andere Konstruktoren laden
-;; bei Klassen könnte zB von Methode noch eine Klasse Methoden abgehen, die auch negative
-;; preconditions haben kann; dann zwischen Klassen umschalten
-;; erstmal mit Struct
+	       
 		       
-
+;;read in hddl-problems and transform them into usable hddl-problem structures
 (defun read-hddl-problem (filename)
   (declare (optimize debug))
   (print "Reading problem-file...")
@@ -129,42 +122,56 @@ read-problem -> *goal-tasks*, *status*, *objects*
     (error "This function can only read a HDDL file - make sure the file you want to read ends in .hddl! "))
   (unless (search "problem" filename) 
     (error "This function can only read a problem-HDDL file - make sure the file you want to read is the problem-file! "))
+  
   (let* ((read-problem (read-file filename))
-	 objects
-	 tasks
-	 ordering
-	 constraints
-	 status
-	 problem
-	 ;; hier nochmal schauen, was wir wegen :htn :tasks zu machen ist!
-	 (look-up '((:objects . objects)(:tasks . tasks)(:ordering . ordering)(:constraints . constraints) (:status . status))))
-    (declare (special objects tasks ordering constraints status problem))
-   (dolist (element (cdr read-problem))
+	 (objects)
+	 (htn)
+	 (status)
+	 (look-up '((:objects . objects)(:htn . htn) (:init . status))))
+    (declare (special objects htn status))
+    (dolist (element (cdr read-problem))
      (let ((key (assoc (first element) look-up)))
        (when key
-	  (set (cdr key) (cons element (symbol-value (cdr key)))))))
+	 (set (cdr key) (cons element (symbol-value (cdr key)))))))
+    (setq htn (cdr (car htn)))
+    ;;(pprint htn) ;;debugging
+    ;;(pprint status) ;;debugging
+
+    ;;second iteration of separating file into components - separate htn into tasks, ordering,constraints; not using look-up-list-method, because the components are not separated into differnet lists already, e.g. a list with first element ":TASKS" and second element a list of tasks, but rather elements in the list beginning with ":HTN")
+    (let ((tasks)
+	  (ordering)
+	  (constraints))
+      
+      (if (equal (string (car htn)) "TASKS")
+	  (setq tasks (second htn)
+		htn (cddr htn)))
+      (if (equal (string (car htn)) "ORDERING")
+	  (setq ordering (second htn)
+		htn (cddr htn)))
+      (if (equal (string (car htn)) "CONSTRAINTS")
+	  (setq constraints (second htn)
+		htn (cddr htn)))
+      ;;(pprint tasks) ;;debugging 
+      ;;(pprint ordering) ;;debugging
+      ;;(pprint constraints) ;;debugging
+
+      ;;remove unnecessary "AND" from task-list:
+      (if (equal (string (car tasks)) "AND")
+	  (setq tasks (cdr tasks))) 
+      ;;(pprint tasks) ;;debugging
+      ;;TODO: maybe find a better solution for htn-separation
 
     ;; transform elements of the domain-lists into the appropriate structures
-    (let (hddl-tasks)
-    ;;objects noch machen: Schwierigkeit: Typen mit - müssen allen davorstehenden Objekten hinzugefügt werden
-    (dolist (element tasks)
-      (destructuring-bind (x name y . parameters) element
-	(declare (ignore x y))
-	(push (make-hddl-task :name name :parameters parameters) hddl-tasks)))
+    (let ((hddl-tasks)
+      (problem))
+    ;;TODO : objects; Schwierigkeit: Typen mit - müssen allen davorstehenden Objekten hinzugefügt werden
+      
+      (setq hddl-tasks
+	    (loop for (name . parameters) in tasks collect
+		   (make-hddl-task :name name :parameters parameters))
+	    problem (make-hddl-problem :name (second read-problem) :domain (cdr (third read-problem)) :objects (cdr (car objects)) :tasks hddl-tasks :ordering ordering :constraints constraints :init-status (cdr (car status))))))))
 
-    (setq problem (make-hddl-problem :name (second problem) :domain (third problem)
-				      :objects objects :tasks tasks :ordering ordering :constraints constraints :init-status status)))
 
-    problem))
-
-#| so könnten alle Elemente entfernt werden, die keine Methoden sind, also nicht mit :method beginnen: (remove-if-not :method HDDL :key #'first)
-oder (find :method HDDL :key #'first) -> dann wird es gefunden
-matching durch destructuring-bind:
-(destructuring-bind ((a b) . rest) '((1 2) (3 4))   (+ a b)) ----> 3
--> findet dann genau die angegebenen Elemente
-im onlist: destructuring-bind mit ?k, also Variablen; so können wir pattern-matchen mit den Variablen und unifizieren; mal im Buch schauen und auch in dessen
-online-Code schauen und Teile herausnehmen
-|#
 
 
 
